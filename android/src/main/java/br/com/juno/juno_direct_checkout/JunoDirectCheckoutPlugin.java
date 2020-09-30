972a37f1399056dbfb87aed16fb58da77f5b652d
Vagner Santos <vagner@produtoraduka.com.br>
2020-09-30 12:41:00 -0300

    Fix Bug Context Null

--- android/src/main/java/br/com/juno/juno_direct_checkout/JunoDirectCheckoutPlugin.java
+++ android/src/main/java/br/com/juno/juno_direct_checkout/JunoDirectCheckoutPlugin.java
@@ -1,5 +1,6 @@
 package br.com.juno.juno_direct_checkout;
 
+import android.app.Activity;
 import android.content.Context;
 
 import androidx.annotation.NonNull;
@@ -16,117 +17,124 @@ import br.com.juno.directcheckout.model.Card;
 import br.com.juno.directcheckout.model.DirectCheckoutListener;
 import br.com.juno.directcheckout.model.DirectCheckoutException;
 
-/** JunoDirectCheckoutPlugin */
+/**
+ * JunoDirectCheckoutPlugin
+ */
 public class JunoDirectCheckoutPlugin implements FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private MethodChannel channel;
-  private Context context;
+    /// The MethodChannel that will the communication between Flutter and native Android
+    ///
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
+    private MethodChannel channel;
+    private Context context;
+    private final Activity activity;
 
-  @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    channel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "juno_direct_checkout");
-    channel.setMethodCallHandler(this);
-    context = flutterPluginBinding.getApplicationContext();
-  }
+    private JunoDirectCheckoutPlugin(Activity activity) {
+        this.activity = activity;
+    }
 
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "juno_direct_checkout");
-    channel.setMethodCallHandler(new JunoDirectCheckoutPlugin());
-  }
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        channel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "juno_direct_checkout");
+        channel.setMethodCallHandler(this);
+        context = flutterPluginBinding.getApplicationContext();
+    }
 
-  @Override
-  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    switch (call.method) {
-      case "init": 
-        Boolean prod = true;
-        if (call.hasArgument("prod")) {
-          prod = call.argument("prod");
+    // This static function is optional and equivalent to onAttachedToEngine. It supports the old
+    // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
+    // plugin registration via this function while apps migrate to use the new Android APIs
+    // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
+    //
+    // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
+    // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
+    // depending on the user's project. onAttachedToEngine or registerWith must both be defined
+    // in the same class.
+    public static void registerWith(Registrar registrar) {
+        final MethodChannel channel = new MethodChannel(registrar.messenger(), "juno_direct_checkout");
+        channel.setMethodCallHandler(new JunoDirectCheckoutPlugin(registrar.activity()));
+    }
+
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+        switch (call.method) {
+            case "init":
+                Boolean prod = true;
+                if (call.hasArgument("prod")) {
+                    prod = call.argument("prod");
+                }
+                InitializeDirectCheckoutListener initializeListener = new InitializeDirectCheckoutListener(result);
+                DirectCheckout.initialize(activity, prod, initializeListener);
+                break;
+            case "getCardHash":
+                CodigoHashDirectCheckoutListener listener = new CodigoHashDirectCheckoutListener(result);
+                DirectCheckout.getCardHash(loadCard(call), listener);
+                break;
+            case "isValidCardNumber":
+                String cardNumber = call.argument("cardNumber");
+                result.success(DirectCheckout.isValidCardNumber(cardNumber));
+                break;
+            case "isValidSecurityCode":
+                cardNumber = call.argument("cardNumber");
+                String securityCode = call.argument("securityCode");
+                result.success(DirectCheckout.isValidSecurityCode(cardNumber, securityCode));
+                break;
+            case "isValidExpireDate":
+                String expirationMonth = call.argument("expirationMonth");
+                String expirationYear = call.argument("expirationYear");
+                result.success(DirectCheckout.isValidExpireDate(expirationMonth, expirationYear));
+                break;
+            case "isValidCardData":
+                try {
+                    result.success(DirectCheckout.isValidCardData(loadCard(call)));
+                } catch (DirectCheckoutException e) {
+                    result.error("isValidCardDataError", e.getMessage(), null);
+                }
+                break;
+            case "getCardType":
+                cardNumber = call.argument("cardNumber");
+                try {
+                    result.success(DirectCheckout.getCardType(cardNumber));
+                } catch (Exception e) {
+                    result.error("isValidCardDataError", e.getMessage(), null);
+                }
+                break;
+            default:
+                result.notImplemented();
         }
-        InitializeDirectCheckoutListener initializeListener = new InitializeDirectCheckoutListener(result);
-        DirectCheckout.initialize(context, prod, initializeListener);
-        break;
-      case "getCardHash":
-        CodigoHashDirectCheckoutListener listener = new CodigoHashDirectCheckoutListener(result);
-        DirectCheckout.getCardHash(loadCard(call), listener);
-        break;
-      case "isValidCardNumber":
-        String cardNumber = call.argument("cardNumber");
-        result.success(DirectCheckout.isValidCardNumber(cardNumber));
-        break;
-      case "isValidSecurityCode":
-        cardNumber = call.argument("cardNumber");
-        String securityCode = call.argument("securityCode");
-        result.success(DirectCheckout.isValidSecurityCode(cardNumber, securityCode));
-        break;
-      case "isValidExpireDate":
-        String expirationMonth = call.argument("expirationMonth");
-        String expirationYear = call.argument("expirationYear");
-        result.success(DirectCheckout.isValidExpireDate(expirationMonth, expirationYear));
-        break;
-      case "isValidCardData":
-        try {
-          result.success(DirectCheckout.isValidCardData(loadCard(call)));
-        } catch (DirectCheckoutException e) {
-          result.error("isValidCardDataError", e.getMessage(), null);
-        }
-        break;
-      case "getCardType":
-        cardNumber = call.argument("cardNumber");
-        try {
-          result.success(DirectCheckout.getCardType(cardNumber));        
-        } catch (Exception e) {
-          result.error("isValidCardDataError", e.getMessage(), null);
-        }
-        break;
-      default:
-        result.notImplemented();
     }
-  }
 
-  @Override
-  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-    channel.setMethodCallHandler(null);
-  }
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
+    }
 
-  private Card loadCard(MethodCall call) {
-    String cardNumber = "";
-    if (call.hasArgument("cardNumber")) {
-        cardNumber = call.argument("cardNumber");
+    private Card loadCard(MethodCall call) {
+        String cardNumber = "";
+        if (call.hasArgument("cardNumber")) {
+            cardNumber = call.argument("cardNumber");
+        }
+        String holderName = "";
+        if (call.hasArgument("holderName")) {
+            holderName = call.argument("holderName");
+        }
+        String securityCode = "";
+        if (call.hasArgument("securityCode")) {
+            securityCode = call.argument("securityCode");
+        }
+        String expirationMonth = "";
+        if (call.hasArgument("expirationMonth")) {
+            expirationMonth = call.argument("expirationMonth");
+        }
+        String expirationYear = "";
+        if (call.hasArgument("expirationYear")) {
+            expirationYear = call.argument("expirationYear");
+        }
+        return new Card(
+                cardNumber,
+                holderName,
+                securityCode,
+                expirationMonth,
+                expirationYear
+        );
     }
-    String holderName = "";
-    if (call.hasArgument("holderName")) {
-        holderName = call.argument("holderName");
-    }
-    String securityCode = "";
-    if (call.hasArgument("securityCode")) {
-        securityCode = call.argument("securityCode");
-    }
-    String expirationMonth = "";
-    if (call.hasArgument("expirationMonth")) {
-        expirationMonth = call.argument("expirationMonth");
-    }
-    String expirationYear = "";
-    if (call.hasArgument("expirationYear")) {
-        expirationYear = call.argument("expirationYear");
-    }
-    return new Card(
-            cardNumber,
-            holderName,
-            securityCode,
-            expirationMonth,
-            expirationYear
-    );
-  }
 }
